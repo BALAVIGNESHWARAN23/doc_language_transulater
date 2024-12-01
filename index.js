985b417f6ffdd9045af850ace5c8dc79586bbde5
BALAVIGNESHWARN.P <137701826+BALAVIGNESHWARAN23@users.noreply.github.com>
2024-12-01 13:29:30 +0530

    update

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+require("./config/passport-setup"); 
+
+const authRoutes = require("./routes/authRoutes");
+const passwordRoutes = require("./routes/passwordRoutes");
+
+const app = express();
+
+// Middleware setup
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"view")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your-session-secret',
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to Database"))
+    .catch(err => console.error("Error in Connecting to Database:", err));
+
+// Define routes
+app.use("/", authRoutes);
+app.use("/", passwordRoutes);
+
+// Route for the homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "view", "indexbg.html"));
+    
+});
+
+app.use((req,res)=>{
+    res.send("<h1>404 page not found</h1>")
+
+})
+
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Listening on PORT 3000");
+});
